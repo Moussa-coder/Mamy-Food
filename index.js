d5c7935f7cc54f2005bc8cf0a50041bcc33df863
Moussa-codeur <correamoussa469@gmail.com>
2025-05-07 22:37:09 +0200

    Mise en place de la videos

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-document.addEventListener('DomContentLoaded', function() {
-    const menuToggle = document.createElement('button');
-    menuToggle.className = 'menu-Toggle';
-    menuToggle.innerHTML = '<span></span><span></span><span></span>';
-
-    const navbar = document.querySelector('.navbar');
-    navbar.appendChild(menuToggle);
-    
-    const navLinks = document.querySelector('.nav-links');
-    
-    menuToggle.addEventListener('click', function() {
-      this.classList.toggle('active');
-      navLinks.classList.toggle('active');
-    });
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav-links').classList.toggle('active');
   });
